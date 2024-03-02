2ed9a7098412855c09376f4a539f8688018a81e7
kemo <kemotherapy@protonmail.com>
2024-03-02 01:58:48 -0500

    add mult program

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,4 +7,41 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// product = 0
+@R2
+M = 0
+// if (R0 == 0) goto END
+@R0
+D = M
+@END
+D;JEQ
+// if (R1 == 0) goto END
+@R1
+D = M
+@END
+D;JEQ
+// i = 0
+@i
+M = 0
+(LOOP)
+// if (i == RAM[1]) goto END
+@i
+D = M
+@R1
+D = D - M
+@END
+D;JEQ
+// product = product + RAM[0]
+@R0
+D = M
+@R2
+M = M + D
+// i = i + 1
+@i
+M = M + 1
+// goto LOOP
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
